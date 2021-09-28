c081cacd505b525b6950b0f5578853c585626856
vanshitamoolchandani <63742296+vanshitamoolchandani@users.noreply.github.com>
2021-09-28 23:41:48 +0530

    done

--- index.js
+++ index.js
@@ -1,30 +1,59 @@
-var cashAmount = document.querySelector("#cash-amount");
-var billAmount = document.querySelector("#bill-amount");
-var checkButton = document.querySelector("#check-button");
-var message = document.querySelector("#error-message");
-
-console.log(billAmount.value);
-checkButton.addEventListener("click", () => console.log("clicked"));
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-button");
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
 
 
-// checkButton.addEventListener("click", () => (console.log("clickes")));
 
-// checkButton.addEventListener("click", function checkBillAndCashAmount(){
-//     if(cashAmount.value > 0){
-//         if(cashAmount.value <= billAmount.value){
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-//         } 
-//         else{
-//             printMassage("Invalid Input. Bill amount should be greater that Cash amount");
-//         }
-//     }
-//     else{
-//         printMassage("Invalid Input. Cash Amount should be positive.");
-//     }
-// });
+checkButton.addEventListener("click", 
+function validateBillAndCashAmount() {
 
+  hideMessage();
 
-// function printMassage(msg){
-//     message.style.display = "block";
-//     message.innerText=message;
-// }
+  if (billAmount.value > 0) 
+  {
+    if ( (Number(billAmount.value)) != (Number(cashGiven.value))) 
+    {
+      if (Number(cashGiven.value) >= Number(billAmount.value)) 
+      {
+        const amountToBeReturned = cashGiven.value - billAmount.value;
+        calculateChange(amountToBeReturned);
+      } 
+      else 
+      {
+        showMessage("Cash Given should not be less than Bill Amount");
+      }
+    }
+    else
+    {
+      showMessage("No change to be returned");
+    }
+   
+  } else {
+    showMessage("Bill Amount should not be less than zero");
+  }
+}
+);
+
+function calculateChange(amountToBeReturned)
+ {
+  for (let i = 0; i < availableNotes.length; i++) {
+    const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+
+    amountToBeReturned = amountToBeReturned % availableNotes[i];
+
+    noOfNotes[i].innerText = numberOfNotes;
+  }
+}
+
+function hideMessage() {
+  message.style.display = "none";
+}
+
+function showMessage(msg) {
+  message.style.display = "block";
+  message.innerText = "Error: " + msg;
+}
